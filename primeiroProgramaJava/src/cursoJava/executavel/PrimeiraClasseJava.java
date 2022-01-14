7b611e9809a57071201c8f18ff127a1d8168f235
alsst <alsst@DESKTOP-H1J6UTP>
2022-01-14 00:53:42 -0200

    primeiro projeto feito com eclipse e commitado no git

---
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursoJava.executavel;
+
+public class PrimeiraClasseJava {
+	/*main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		/*escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+		System.out.println("Java é orientado a objetos");
+	}
+}
