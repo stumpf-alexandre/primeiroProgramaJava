34d2944326df80553054ea7c92db70c8e80c6fd5
alsst <alsst@DESKTOP-H1J6UTP>
2022-01-14 11:59:36 -0200

    teste token

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,7 +4,11 @@ public class PrimeiraClasseJava {
 	/*main é um metodo auto executavel em java*/
 	public static void main(String[] args) {
 		/*escreve texto no console*/
+		/*teste de commit com o novo token*/
 		System.out.println("Meu primeiro programa em Java");
+		
 		System.out.println("Java é orientado a objetos");
+		
+		System.out.println("Teste de token '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'");
 	}
 }
